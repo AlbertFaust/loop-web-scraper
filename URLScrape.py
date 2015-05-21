9a9081b49c8da10ba97bf428f97ab4e6a86889b1
Albert Faust <albertfaust1504@gmail.com>
2015-05-21 23:27:24 +0100

    Create URLScrape.py
    Script to pull the URLs from github.com/AlbertFaust?tab=repositories. The purpose of this script is purely for learning how to web scrape using BeautifulSoup.

---
+++ URLScrape.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import urllib2
+url="http://github.com/AlbertFaust?tab=repositories"
+page=urllib2.urlopen(url)
+soup = BeautifulSoup(page.read())
+for link in soup.find_all('a'):
+	print(link.get('href'))
