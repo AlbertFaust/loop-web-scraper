33d92c7aee9ec74cc092ea7ac1a4205956e7fa0d
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-06-02 14:34:31 +0100

    Update URLScrape.py
    Changed urllib2 to urllib.request (python3)

--- URLScrape.py
+++ URLScrape.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
-import urllib2
+from urllib.request import urlopen
 url="http://github.com/AlbertFaust?tab=repositories"
-page=urllib2.urlopen(url)
+page=urlopen(url)
 soup = BeautifulSoup(page.read())
 for link in soup.find_all('a'):
 	print(link.get('href'))
