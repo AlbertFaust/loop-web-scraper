28620dd8af3df58355853a9d9efe476285d004f7
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-06-02 16:19:53 +0100

    Update URLScrape.py
    Added the ability to recursivly go through urls. Still not fully functional, needs more detailed logic as making too many assumptions about input.

--- URLScrape.py
+++ URLScrape.py
@@ -1,8 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3                                                                                                                                        
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 url="http://github.com/AlbertFaust?tab=repositories"
-page=urlopen(url)
-soup = BeautifulSoup(page.read())
-for link in soup.find_all('a'):
-	print(link.get('href'))
+
+def recursiveURL(url,depth):
+    print(url)
+    if depth == 5 or url is None:
+        return url
+    else:
+        page=urlopen(url)
+        soup=BeautifulSoup(page.read())
+        new = soup.find('a')
+        if len(new) == 0:
+            return url
+        else:
+            return url, recursiveURL(new, depth+1)
+def links(url):
+    page=urlopen(url)
+    soup=BeautifulSoup(page.read())
+    link=soup.find_all('a')
+    for i in link:
+        link.append(recursiveURL(i,0))
+        return link
+
+recursiveURL(url,1)
+print(links(url))
