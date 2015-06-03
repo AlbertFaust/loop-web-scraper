98d99d7931ea8e47f21af4ca2247e85eb34e37bf
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-06-03 15:40:12 +0100

    Update URLScrape.py
    Started over, seems to be working much better.

--- URLScrape.py
+++ URLScrape.py
@@ -1,27 +1,15 @@
-#!/usr/bin/env python3                                                                                                                                        
+#! /usr/bin/bash python3
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-url="http://github.com/AlbertFaust?tab=repositories"
-
-def recursiveURL(url,depth):
-    print(url)
-    if depth == 5 or url is None:
-        return url
-    else:
-        page=urlopen(url)
+url='http://github.com/AlbertFaust?tab=repositories'
+page=urlopen(url)
+soup=BeautifulSoup(page.read())
+for links in soup.find_all('a'):
+    if (links.get('href')).startswith('http') and (links.get('href')) != '#':
+        print(links.get('href'))                                                                                                                                
+        page=urlopen(links.get('href'))
         soup=BeautifulSoup(page.read())
-        new = soup.find('a')
-        if len(new) == 0:
-            return url
-        else:
-            return url, recursiveURL(new, depth+1)
-def links(url):
-    page=urlopen(url)
-    soup=BeautifulSoup(page.read())
-    link=soup.find_all('a')
-    for i in link:
-        link.append(recursiveURL(i,0))
-        return link
-
-recursiveURL(url,1)
-print(links(url))
+        for links in soup.find_all('a'):
+            print(links.get('href'))
+    else:
+        print('http://github.com'+links.get('href'))
